e4d2317b1240d5cc663ba467c77c33049b301fac
Rogelio Vargas Marquez <rogelio.vargas.marquez@gmail.com>
2024-01-03 23:25:50 -0600

    fix(notfound): added basic not found page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withNextIntl = require('next-intl/plugin')();
  
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
+  trailingSlash: true,
 }
 
 module.exports = withNextIntl(nextConfig);
--- src/middleware.js
+++ src/middleware.js
@@ -11,9 +11,15 @@ export default createMiddleware({
  
 export const config = {
   matcher: [
+    // Enable a redirect to a matching locale at the root
     '/',
+
     // Set a cookie to remember the previous locale for
     // all requests that have a locale prefix
     '/(es|en)/:path*',
+
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!_next|_vercel|.*\\..*).*)'
   ]
 };
